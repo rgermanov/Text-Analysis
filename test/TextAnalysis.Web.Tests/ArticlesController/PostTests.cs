3cd0835fac9703d0c5d73050dba123bd05c5248d
ruslan <ruslan@germanov.com>
2016-09-21 22:27:05 +0200

    added the web project to the tests

---
+++ test/TextAnalysis.Web.Tests/ArticlesController/PostTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TextAnalysis.Web.Tests.ArticlesController
+{
+    public class PostTests 
+    {
+        [Fact]
+        public void ErrorWhenEmptyUrl()
+        {
+            Assert.True(false);
+        }
+    }
+}
